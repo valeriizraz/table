ddd0f7b5f6a312d61c4277de03ad0cfb93239982
valeriizraz <valeriizraz@gmail.com>
2023-10-05 15:42:42 +0200

    init table

---
+++ main.js
@@ -0,0 +1,130 @@
+'use strict';
+
+{
+const array = [
+  {
+    "id": 253842678,
+    "title": "Смартфон Xiaomi 11T 8/128GB",
+    "price": 27000,
+    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
+    "category": "mobile-phone",
+    "discont": false,
+    "count": 3,
+    "units": "шт",
+    "images": {
+      "small": "img/smrtxiaomi11t-m.jpg",
+      "big": "img/smrtxiaomi11t-b.jpg"
+    }
+  },
+  {
+    "id": 296378448,
+    "title": "Радиоуправляемый автомобиль Cheetan",
+    "price": 4000,
+    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
+    "category": "toys",
+    "discont": 5,
+    "count": 1,
+    "units": "шт",
+    "images": {
+      "small": "img/cheetancar-m.jpg",
+      "big": "img/cheetancar-b.jpg"
+    }
+  },
+  {
+    "id": 215796548,
+    "title": "ТВ приставка MECOOL KI",
+    "price": 12400,
+    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
+    "category": "tv-box",
+    "discont": 15,
+    "count": 4,
+    "units": "шт",
+    "images": {
+      "small": "img/tvboxmecool-m.jpg",
+      "big": "img/tvboxmecool-b.jpg"
+    }
+  },
+  {
+    "id": 246258248,
+    "title": "Витая пара PROConnect 01-0043-3-25",
+    "price": 22,
+    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
+    "category": "cables",
+    "discont": false,
+    "count": 420,
+    "units": "v",
+    "images": {
+      "small": "img/lan_proconnect43-3-25.jpg",
+      "big": "img/lan_proconnect43-3-25-b.jpg"
+    }
+  }
+]
+
+const getTable = () => {
+  const table = document.createElement('table');
+
+  return table;
+};
+
+const getTableTr = () => {
+  const tr = document.createElement('tr');
+
+  return tr;
+};
+
+const getTableTd = () => {
+  const td = document.createElement('td');
+
+  return td;
+};
+
+// const createRow = (arr) => {
+//   const table = getTable();
+  
+//   for (let i = 0; i < arr.length; i++) {
+//     const tr = getTableTr();
+//     table.append(tr);
+//   }
+
+//   return table;
+// };
+
+// console.log(createRow(array));
+
+const renderGoods = (arr) => {
+  const table = getTable();
+
+  const newObj = arr.map((elem) => {
+    const tr = getTableTr();
+    table.append(tr);
+  
+    for (let item of Object.values(elem)) {
+    const td = getTableTd();
+    td.append(item);
+    tr.append(td);
+    
+      if (typeof item === 'object') {
+        const keys = Object.values(item);
+        td.append(keys);
+        
+      } 
+    }
+  
+    
+  })
+
+  return table;
+}
+
+const go = (play) => {
+  const start = document.querySelector(play);
+  const table = renderGoods(array);
+  
+  start.append(table);
+  console.log(start)
+  console.log(table);
+}
+
+window.tbl = go;
+}
+
